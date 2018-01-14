c3273d2f687317e57e90e12643836765174acfe1
Shane White <shane@midweststreams.com>
2018-01-14 16:25:12 -0600

    Added tamper monkey script

---
+++ tamper_monkey_script.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Old Drip
+// @namespace    https://www.getdrip.com/*
+// @version      0.1
+// @description  Bringing back the old version of drip
+// @author       Shane White
+// @match        https://www.getdrip.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+       $( "link" ).each(function( index ) {
+       var currentCSS = $(this).attr("href");
+           if (currentCSS.indexOf("application") >= 0)
+           {
+               $(this).attr("href", "https://shanefromfargo.github.io/drip-reskin/OldDrip.css")
+           }
+           if (currentCSS.indexOf("favicon") >= 0)
+           {
+               $(this).attr("href", "https://shanefromfargo.github.io/drip-reskin/favicon.ico")
+           }
+       });
+        $(".logo-picker").remove();
+
+
+    // Your code here...
+})();
